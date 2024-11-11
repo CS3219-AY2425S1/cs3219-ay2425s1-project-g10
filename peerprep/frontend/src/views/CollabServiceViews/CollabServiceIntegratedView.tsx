d53e9e75ed2b46db56b7ed799851825777f6876b
Suryansh Kushwaha <112935210+suryanshkushwaha@users.noreply.github.com>
2024-11-11 22:13:26 +0800

    minor spelling error

--- peerprep/frontend/src/views/CollabServiceViews/CollabServiceIntegratedView.tsx
+++ peerprep/frontend/src/views/CollabServiceViews/CollabServiceIntegratedView.tsx
@@ -406,7 +406,7 @@ const CollaborationServiceIntegratedView: React.FC = () => {
                 onClick={handleAssessCode}
                 className="px-4 py-2 bg-blue-500 text-white rounded text-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
               >
-                Access
+                Assess
               </button>
             </div>
 
